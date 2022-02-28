4fa66c6ebcb15abb5d542866025028d39caf56ce
Gustavo de Castro Biage <gustavo.c.biage@gmail.com>
2022-02-27 21:25:29 -0300

    Correção em alertas de erro Roulette.

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -68,7 +68,7 @@ abstract contract Roulette {
     /**
      * Registra a execução de fim de contrato
      */
-    bool endedAlreadyExecuted;
+    bool public endedAlreadyExecuted;
 
     /**
      * Construtor;
@@ -77,11 +77,9 @@ abstract contract Roulette {
      * @param tax_ Define a taxa de cada aposta que gera lucro para o proprietário do contrato.
      * @param duration Duração que o contrato se encontra aberto para apostas
      */
-    constructor(uint c, uint token_, uint tax_, uint duration) payable {
+    constructor(uint c, uint token_, uint tax_, uint duration) {
         owner = payable(msg.sender);
         nColors = (1 << c);
-        totalDonation = msg.value;
-        totalPrize = msg.value;
         token = token_;
         tax = tax_;
         for( uint i = 0; i < nColors; i++){
@@ -101,11 +99,11 @@ abstract contract Roulette {
 
     function makeBet(uint color) external payable {
         // Verifica se o contrato ainda é válido
-        assert(block.number <= validUntil);
+        require(block.number <= validUntil, "Fora do periodo de apostas");
         // Verifica que o valor transferido compra pelo menos uma ficha.
-        assert(msg.value >= token + tax);
+        require(msg.value >= token + tax, "Transferencia insuficiente");
         // Verifica que este endereço não tem um aposta feita em uma outra cor.
-        assert((bets[msg.sender].registered && bets[msg.sender].color == color) || !bets[msg.sender].registered);
+        require((bets[msg.sender].registered && bets[msg.sender].color == color) || !bets[msg.sender].registered, "Aposta ja realizada em outra cor");
         // Retira a taxa e compra as fichas.
         totalTax += tax;
         uint quotient = (msg.value - tax) / token; // divisão de inteiro
@@ -130,9 +128,9 @@ abstract contract Roulette {
         // Termina o contrato caso o número máximo de blocos tenha passado
     }
 
-    function endContract() public {
-        assert(!endedAlreadyExecuted);
-        assert(hasEnded());
+    function endContract() external {
+        require(!endedAlreadyExecuted, "O fim do contrato foi executado.");
+        require(hasEnded(), "O contrato ainda nao terminou.");
         uint selected = selectColor();
 
         // Distribui a premiação aos ganhadores
@@ -153,6 +151,7 @@ abstract contract Roulette {
         }
         // Envia a parte do dono do contrato
         owner.transfer(totalTax);
+        endedAlreadyExecuted = true;
     }
 
     function selectColor() internal virtual returns(uint);
@@ -160,13 +159,13 @@ abstract contract Roulette {
     function hasEnded() public virtual returns(bool);
 
     function donate() external payable {
-        assert(!hasEnded())
+        require(!hasEnded(), "Contrato ja terminou.");
         totalPrize += msg.value;
         totalDonation += msg.value;
     }
 
     function bettedOnColor(uint colorId) external view returns(uint){
-        assert(colorId < nColors);
+        require(colorId < nColors, "Cor invalida.");
         return colors[colorId].total;
     }
 }
